5e0b15354efe494b2371c88c4e4f8c76b62fa47f
KilianDunat <kilian.dunat@hotmail.fr>
2023-03-20 21:36:58 +0100

    update

--- Requête SQL/SQLQuery_wsus.sql
+++ Requête SQL/SQLQuery_wsus.sql
@@ -1,4 +1,6 @@
-Select cd.ComputerModel as Mod�le, cd.BiosVersion, c.FullDomainName as Nom, c.LastReportedStatusTime
+/* Requête SQL pour récupérer les clé et mot de passe BitLocker */
+
+Select cd.ComputerModel as Mod�le, cd.BiosVersion, c.FullDomainName as Nom, c.LastReportedStatusTime
 From tbComputerTargetDetail as cd
 Inner Join tbComputerTarget as c
 on cd.TargetID = c.TargetID
